78dd212a1c4cca5208b89d761735bb850e7dc302
tinhph.nxbgd@gmail.com <tinhph.nxbgd@gmail.com>
2018-06-26 09:51:03 +0700

    commit messages

--- src/com/education/Main.java
+++ src/com/education/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello i'm java");
+        System.out.println("Xin chao Long");
     }
 }
